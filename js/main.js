ffcd5deac1ec01031d7f6c3c563fcc00e81bb574
iasd-vdp <59889538+iasd-vdp@users.noreply.github.com>
2020-02-06 07:55:09 -0300

    06/02/2020

--- js/main.js
+++ js/main.js
@@ -189,7 +189,7 @@
 	/*----------------------------------------------------*/
 	/* Final Countdown Settings
 	------------------------------------------------------ */
-	var finalDate = '2020/04/05';
+	var finalDate = '2020/03/08';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
